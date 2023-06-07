b0f676f2714aa0c169d43a2b6e59523285ecba0b
RealMuffinTime <realmuffintime@gmail.com>
2023-06-07 16:28:56 +0200

    update www and add bot subdomain

---
+++ footer.php
@@ -0,0 +1,4 @@
+<footer>
+    <hr>
+    <p>Made with <span alt="love"><3</span> by MuffinTime.</p>
+</footer>
---
+++ header.php
@@ -0,0 +1,6 @@
+
+<header>
+    <p>Hello there, It's MuffinTime.</p>
+    <p>Welcome unknown stranger from the depths of the internet.</p>
+    <hr>
+</header>
