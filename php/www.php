6a818e5ad3b1a98baeb782fc9ba82d85f66f4105
RealMuffinTime <realmuffintime@gmail.com>
2025-07-31 15:17:59 +0200

    update main page

--- php/www.php
+++ php/www.php
@@ -11,20 +11,21 @@
         ?>
         <main>
             <article>
-                <h2>MuffinTime Bots</h2> 
-                <a href="<?php echo protocol() . 'bots.' . $_SERVER['SERVER_NAME']?>">bots.<?=$_SERVER['SERVER_NAME']?></a>
+                <h2>Some Minecraft related projects</h2>
+                BlueMap Banners <a href="https://modrinth.com/mod/bluemap-banners" target="_blank" rel="noopener">View Mod on Modrinth</a> - <a href="https://github.com/RealMuffinTime/bluemap-banners" target="_blank" rel="noopener">View source on GitHub</a><br>
+                The MuffinTime Recourcepack <a href="https://modrinth.com/datapack/muffintime-resource-pack" target="_blank" rel="noopener">View Pack on Modrinth</a> - <a href="https://github.com/RealMuffinTime/muffintime-resource-pack" target="_blank" rel="noopener">View source on GitHub</a><br>
+                The MuffinTime Datapack <a href="https://modrinth.com/datapack/muffintime-data-pack" target="_blank" rel="noopener">View Pack on Modrinth</a> - <a href="https://github.com/RealMuffinTime/muffintime-data-pack" target="_blank" rel="noopener">View source on GitHub</a><br>
             </article>
             <article>
                 <h2>The MuffinTime Minecraft</h2> 
                 Server adresss <buttton tabindex="0"><code>mc.muffintime.tk</code></buttton><br>
                 Map <a href="<?php echo protocol() . 'mc.' . $_SERVER['SERVER_NAME']?>">mc.<?=$_SERVER['SERVER_NAME']?></a><br>
+                Statistics <a href="<?php echo protocol() . 'mc-stats.' . $_SERVER['SERVER_NAME']?>">mc-stats.<?=$_SERVER['SERVER_NAME']?></a><br>
                 Discord <a href="https://discord.gg/mKBhEBgZfa">discord.gg/mKBhEBgZfa</a>
             </article>
             <article>
-                <h2>The MuffinTime Packs</h2> 
-                The MuffinTime Recourcepack <a href="https://modrinth.com/datapack/muffintime-resource-pack" target="_blank" rel="noopener">View Pack on Modrinth</a> - <a href="https://github.com/RealMuffinTime/muffintime-resource-pack" target="_blank" rel="noopener">View source on GitHub</a><br>
-                The MuffinTime Datapack <a href="https://modrinth.com/datapack/muffintime-data-pack" target="_blank" rel="noopener">View Pack on Modrinth</a> - <a href="https://github.com/RealMuffinTime/muffintime-data-pack" target="_blank" rel="noopener">View source on GitHub</a><br>
-                The MuffinTime Minecraft <a href="https://modrinth.com/collection/jmpgwvc1" target="_blank" rel="noopener">View Collection on Modrinth</a><br>
+                <h2>Some Discord bots</h2> 
+                <a href="<?php echo protocol() . 'bots.' . $_SERVER['SERVER_NAME']?>">bots.<?=$_SERVER['SERVER_NAME']?></a>
             </article>
             <article>
                 <h2>A customized Garry's Mod loading screen</h2>
